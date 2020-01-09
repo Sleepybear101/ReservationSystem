9c57faaf789964d590a1010d94f61712007f7f85
Roy Heimel <Royheimel@hotmail.com>
2020-01-09 12:54:40 +0100

    poek

---
+++ CostumeTest/Connection/SqlConnection.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace ReservationGuest
+{
+    public class SqlConnection
+    {
+
+        private MySqlConnection _con;
+        public MySqlCommand Cmd;
+        private MySqlDataAdapter _da;
+        private DataTable _dt;
+
+
+        //Query wordt gemaakt naar command
+        public void SqlQuery(string queryText)
+        {
+            _con = new MySqlConnection("Server=localhost;  Database=reservationsystem; username=root; password=");
+            _con.Open();
+            Cmd = new MySqlCommand(queryText, _con);
+        }
+        //Query wordt uitgevoerd en krijgt informatie terug
+        public DataTable QueryEx()
+        {
+            _da = new MySqlDataAdapter(Cmd);
+            _dt = new DataTable();
+            _da.Fill(_dt);
+
+            return _dt;
+        }
+        //Query wordt uitgevoerd met geen resultaat
+        public void NonQueryEx()
+        {
+            Cmd.ExecuteNonQuery();
+        }
+    }
+}
--- CostumeTest/wwwroot/js/button.js
+++ CostumeTest/wwwroot/js/button.js
@@ -1,18 +1,46 @@
-﻿var number = 2
-
-function CreateAlert() {
-    $("div[id=div1").hide();
-    var n = number.toString();
-    $("div[id=div"+n).removeClass('CHidden');
-
-
-
-}
-
-function NextDiv(){
+﻿
+function LastDiv() {
     $("div[class=divs]").hide();
-    number = 3;
+    $("div[id=div4]").hide();
+    number -= 1;
     var n = number.toString();
-    $("div[id=div"+n).removeClass('CHidden');
-    $("div[id=div"+n+"]").show();
+    $("div[id=div" + n + "]").removeClass('CHidden');
+    $("div[id=div" + n + "]").show();
+
 }
+var number = 1;
+function NextDiv() {
+    $("div[class=divs]").hide();
+    $("div[id=div4]").hide();
+    number += 1;
+    $("div[id=div"+n+"]").removeClass('CHidden');
+    $("div[id=div" + n + "]").show();
+    var n = number.toString();
+    if (number == 2) {
+        datumprikker();
+    }
+
+}
+/*
+function datumprikker() {
+
+    function() {
+        $('#datetimepicker').datetimepicker({
+            inline: true,
+            sideBySide: true
+        });
+    }
+
+        $('#datetimepicker').on('dp.change', function (event) {
+            //console.log(moment(event.date).format('MM/DD/YYYY h:mm a'));
+            //console.log(event.date.format('MM/DD/YYYY h:mm a'));
+            $('#selected-date').text(event.date);
+            var formatted_date = event.date.format('MM/DD/YYYY h:mm a');
+            $('#my_hidden_input').val(formatted_date);
+            $('#hidden-val').text($('#my_hidden_input').val());
+        });
+
+    NextDiv();
+
+}
+*/
