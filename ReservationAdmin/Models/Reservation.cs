5c9aad3c146e2e8b3b3151676678dbbbb5dc4ffb
wurud salih <wu.rud@hotmail.com>
2020-01-15 17:38:07 +0100

    Admin/res

--- ReservationAdmin/Models/Reservation.cs
+++ ReservationAdmin/Models/Reservation.cs
@@ -7,13 +7,13 @@ namespace ReservationAdmin.Models
 {
     public class Reservation
     {
+        public int id { get; set; }
         public string FullName { get; set; }
-
         public string Email { get; set; }
-        public string Amount { get; set; }
+        public int Amount { get; set; }
         public DateTime date { get; set; }
         public string Daypart { get; set; }
         public int Table { get; set; }
-
+        public int IsBlocked { get; set; }
     }
 }
---
+++ ReservationAdmin/wwwroot/js/button.js
@@ -0,0 +1,8 @@
+﻿function Disabled() {
+    $(".TableWitTwo").addClass('Disabled');
+    $(".TableWitFour").addClass('Disabled');
+
+}
+function Message() {
+    alert("Reservering is aangemaakt");
+}
