b9b62a8bdde95bbc81908f6e76b51f3fbc608b9a
wurud salih <wu.rud@hotmail.com>
2020-01-15 18:05:09 +0100

    ClassGuest

---
+++ ReservationAdmin/Models/Guest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReservationAdmin.Models
+{
+    public class Guest
+    {
+        public int id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public int IsBlocked { get; set; }
+    }
+}
